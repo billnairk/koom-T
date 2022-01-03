da81705ec40dfc471f3084872f825af663bb698c
billnairk <billnairk@gmail.com>
2022-01-03 15:55:30 +0900

    public folder add

--- src/server.js
+++ src/server.js
@@ -2,4 +2,9 @@ import express from "express";
 
 const app = express();
 
+app.set("view engine", "pug");
+app.set("views", __dirname + "/public/views");
+app.use("/public", express.static(__dirname + "/public"));
+app.get("/", (_, res) => res.render("home"));
+
 app.listen(3000, console.log("http://localhost:3000/"));
